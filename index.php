24b66c150f288167aa2eacdf19c840c4aa8f0b29
Sparteee <raphaelvictor.pro@hotmail.com>
2023-11-13 11:38:21 +0100

    header style

--- index.php
+++ index.php
@@ -6,11 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Serveur LP MIAW</title>
+    <link rel="stylesheet" href="public/scss/master.css">
 </head>
 <body>
     <header>
         <img src="public/img/imgULR.png" alt="Université de La Rochelle">
         <img src="public/img/imgEDW.png" alt="Ecole du Web">
     </header>
+    <main class="principal">
+        <section class="principal-text">
+            <h1 class="principal-text__title">LP MIAW</h1>
+            <h2 class="principal-text__subtitle">École du Web</h2>
+            <p class="principal-text__formation">Métiers de l'informatique : Applications Web</p>
+            <p class="principal-text__parcours"><strong>Web Designer Intégrateur - Développeur Full Stack</strong></p>
+        </section>
+        <section class="principal-img">
+            <img class="principal-img__image" src="" alt="">
+        </section>
+    </main>
+    <footer>
+        <p>Copyright text & Mentions légales</p>
+        <article>
+            <img src="" alt="linkedin">
+            <img src="" alt="instagram">
+        </article>
+    </footer>
 </body>
 </html>
