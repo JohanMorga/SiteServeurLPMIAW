9159898e6c74c68082b2a767d0cb34d4724f491d
Sparteee <raphaelvictor.pro@hotmail.com>
2023-11-13 12:34:46 +0100

    Nav menu + Text

--- index.php
+++ index.php
@@ -7,21 +7,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Serveur LP MIAW</title>
     <link rel="stylesheet" href="public/scss/master.css">
+    <script src="https://kit.fontawesome.com/0d76749f2d.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header>
+    <header class="header-home">
         <img src="public/img/imgULR.png" alt="Université de La Rochelle">
         <img src="public/img/imgEDW.png" alt="Ecole du Web">
     </header>
+    <nav>
+        <ul class="nav-menu">
+            <li class="nav-menu__icon nav-menu__icon--active"><i class="fa-solid fa-house"></i></li>
+            <li class="nav-menu__icon"><i class="fa-solid fa-graduation-cap"></i></li>
+        </ul>
+    </nav>
     <main class="principal">
         <section class="principal-text">
             <h1 class="principal-text__title">LP MIAW</h1>
             <h2 class="principal-text__subtitle">École du Web</h2>
+            <h3 class="principal-text__serveur">Serveur Pédagogique</h3>
             <p class="principal-text__formation">Métiers de l'informatique : Applications Web</p>
             <p class="principal-text__parcours"><strong>Web Designer Intégrateur - Développeur Full Stack</strong></p>
         </section>
         <section class="principal-img">
-            <img class="principal-img__image" src="" alt="">
+            <img class="principal-img__image" src="public/img/imageAccueil.png" alt="">
         </section>
     </main>
     <footer>
