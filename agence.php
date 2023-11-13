3e09138915e70e6b1060d257cba3985bdaa327f3
Johan Morga <74605758+JohanMorga@users.noreply.github.com>
2023-11-13 12:26:07 +0100

    add html structure

---
+++ agence.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/scss/master.css">
+    <title>Agences des étudiants</title>
+
+</head>
+<body>
+<header>
+    <h1>Les agences créées par les étudiants de la licence professionnelle
+    </h1>
+</header>
+<main>
+    <section class="projets">
+        <h2 class="projets__title">Pour les projets tuteurés - 2023/2024</h2>
+        <ul class="projets__agences">
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Artfull-code</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Lipsom</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Lotus-realisation</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Moderniweb</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Pawweb</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Studio-fmr</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Verseinnovate</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Webforgeurs</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Webmarmottes</a>
+            </li>
+            <li class="agence">
+                <a href="#" class="agence__link">
+                <img class="agence__image" src="" alt="">
+                Wizeus</a>
+            </li>
+        </ul>
+    </section>
+</main>
+</body>
+</html>
