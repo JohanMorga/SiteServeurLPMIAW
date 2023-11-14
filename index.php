77d4dead6a20bad6cc8c729e7cef7e1210d340a8
Sparteee <raphaelvictor.pro@hotmail.com>
2023-11-14 14:49:08 +0100

    finish desktop index page

--- index.php
+++ index.php
@@ -16,8 +16,17 @@
     </header>
     <nav>
         <ul class="nav-menu">
-            <li class="nav-menu__icon nav-menu__icon--active"><i class="fa-solid fa-house"></i></li>
-            <li class="nav-menu__icon"><i class="fa-solid fa-graduation-cap"></i></li>
+            <li class="nav-menu__element">
+                <a class="nav-menu__link nav-menu__link--active" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home">
+                        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+                <p class="nav-menu__text nav-menu__text--inactive">Home</p></a>
+            </li>
+            <li class="nav-menu__element">
+                <a class="nav-menu__link" href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-graduation-cap"><path d="M22 10v6M2 10l10-5 10 5-10 5z"/><path d="M6 12v5c3 3 9 3 12 0v-5"/></svg>
+                <p class="nav-menu__text nav-menu__text--active">Agences</p> </a>
+            </li>
         </ul>
     </nav>
     <main class="principal">
@@ -32,12 +41,16 @@
             <img class="principal-img__image" src="public/img/imageAccueil.png" alt="">
         </section>
     </main>
-    <footer>
-        <p>Copyright text & Mentions légales</p>
-        <article>
-            <img src="" alt="linkedin">
-            <img src="" alt="instagram">
-        </article>
+    <footer class="footer">
+        <p class="footer__text">Copyright text & Mentions légales</p>
+        <ul class="footer-reso">
+            <li class="footer-reso__element">
+                <a class="footer-reso__link" href=""><i class="fa-brands fa-linkedin"></i></a>
+            </li>
+            <li class="footer-reso__element">
+                <a class="footer-reso__link" href=""><i class="fa-brands fa-instagram"></i></a>
+            </li>
+        </ul>
     </footer>
 </body>
 </html>
